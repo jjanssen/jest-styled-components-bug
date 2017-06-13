6c30202e3a428f94091135ae67547d1ccd7adbdc
Janneke Janssen <j.janssen@lukkien.com>
2017-06-13 19:53:07 +0200

    Added a simpler example and react-test-renderer

---
+++ src/Example2.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const ExampleElem2 = styled.div`
+    width: 100%;
+    margin: 10px;
+    padding: 10px;
+`;
+
+export default ExampleElem2;
--- src/__tests__/Example2.test.js
+++ src/__tests__/Example2.test.js
@@ -3,17 +3,17 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { mount } from 'enzyme';
 
-import Example from '../Example';
+import Example2 from '../Example2';
 
-describe('Example', () => {
+describe('Example2', () => {
     it('enzyme: should have the correct properties', () => {
-        const tree = mount(<Example />);
+        const tree = mount(<Example2 />);
         expect(tree).toHaveStyleRule('width', '100%');
         expect(tree).toHaveStyleRule('padding', '10px');
     });
 
     it('react-test-renderer: should have the correct properties', () => {
-        const tree = ReactTestRenderer.create(<Example />).toJSON();
+        const tree = renderer.create(<Example2 />).toJSON();
         expect(tree).toHaveStyleRule('width', '100%');
         expect(tree).toHaveStyleRule('padding', '10px');
     });
